d9a754042bc9f96f56d370e93962c494a32605eb
youuijin <uijin0508@gmail.com>
2023-10-31 01:01:05 +0900

    edit: nam_step.py

--- nam_step.py
+++ nam_step.py
@@ -47,8 +47,10 @@ def main(args):
     
     optim = torch.optim.Adam(model.parameters(), lr=args.lr)
     adv_optim = torch.optim.Adam(model.parameters(), lr=args.lr*args.lr_ratio)
-    
-    writer = SummaryWriter(f"./step_log/step{args.step}_{args.lr}")
+    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer=optim, lr_lambda=lambda epoch: 0.95**epoch)
+    adv_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer=adv_optim, lr_lambda=lambda epoch: 0.95**epoch)
+
+    writer = SummaryWriter(f"./step_log/step{args.step}_{args.train_eps}_{args.lr}")
 
     for epoch in range(args.epoch):
         db = torch.utils.data.DataLoader(train_data, batch_size=args.task_num, shuffle=True, num_workers=2)
@@ -69,9 +71,9 @@ def main(args):
             outputs = torch.argmax(pred, dim=1)
             train_correct_count += (outputs == y).sum().item()
             loss = torch.nn.functional.cross_entropy(logit, y)
-            train_loss += loss*x.shape[0]
+            train_loss += loss.item()*x.shape[0]
             optim.zero_grad()
-            loss.backward()
+            loss.backward(retain_graph=True)
             optim.step()
 
             y_onehot = F.one_hot(y, num_classes = pred.shape[1])
@@ -80,17 +82,17 @@ def main(args):
             if args.step==1:
                 # Quadratic upper bound
                 # L(h(x')) <= L(h(x))+(h(x')-h(x))L'(h(x)) + K/2*||h(x')-h(x)||^2
-                at = utils.setAttak("PGD_Linf", model, args.train_eps, args)
+                at = utils.setAttack("PGD_Linf", model, args.train_eps, args)
                 x_adv = at.perturb(x, y)
-                # logit = model(x)
+                logit = model(x)
                 logit_adv = model(x_adv)
-                # softmax = F.softmax(logit, dim=1)
+                softmax = F.softmax(logit, dim=1)
                 norm = torch.norm(logit_adv-logit)
-                # loss = torch.nn.functional.cross_entropy(logit, y, reduction='none')
+                loss = torch.nn.functional.cross_entropy(logit, y, reduction='none')
                 loss_adv = torch.nn.functional.cross_entropy(logit_adv, y, reduction='none')
 
                 # cross entropy 미분 -> y'-y
-                approx_loss = loss + torch.sum((logit_adv-logit), (pred-y_onehot), dim=1) + args.lipschitz/2.0*torch.pow(norm, 2)
+                approx_loss = loss + torch.sum((logit_adv-logit)*(softmax-y_onehot), dim=1) + args.lipschitz/2.0*torch.pow(norm, 2)
 
                 bound_rate_elements += x.shape[0]
                 bound_rate += (loss_adv<=approx_loss).sum().item() 
@@ -98,30 +100,30 @@ def main(args):
             elif args.step==2:
                 # h(x')에 대해 미분한 결과=0이 되는 성질 이용
                 # ||h(x')-(h(x)-1/K*L'(h(x)))||^2
-                at = utils.setAttak("PGD_Linf", model, args.train_eps, args)
+                at = utils.setAttack("PGD_Linf", model, args.train_eps, args)
                 x_adv = at.perturb(x, y)
-                # logit = model(x)
+                logit = model(x)
                 logit_adv = model(x_adv)
-                # pred = F.softmax(logit, dim=1)
+                softmax = F.softmax(logit, dim=1)
 
                 # cross entropy 미분 -> y'-y
-                approx_loss = torch.pow(torch.norm(logit_adv - (logit - 1.0/args.lipschitz*(pred-y_onehot))),2)
+                approx_loss = torch.pow(torch.norm(logit_adv - (logit - 1.0/args.lipschitz*(softmax-y_onehot))),2)
             
             elif args.step==3: 
                 # approximation #1-1
                 # linear approximation
                 # ||delta*h'(x)+1/K*L'(h(x))||^2
                 
-                at = utils.setAttak("PGD_Linf", model, args.train_eps, args)
+                at = utils.setAttack("PGD_Linf", model, args.train_eps, args)
                 x_adv = at.perturb(x, y)
                 ##################################################
                 # delta를 정확히 계산
-                delta = torch.reshape((x_adv - x), (x.shape[0],-1))
+                delta = torch.reshape((x_adv - x), (x.shape[0],-1)).to(device)
                 # delta를 그냥 eps로 계산
 
                 ##################################################
-                # logit = model()
-                # softmax = F.softmax(logit, dim=1)
+                logit = model(x)
+                softmax = F.softmax(logit, dim=1)
 
                 # calculate jacobian
                 x_copy = x.detach().clone().requires_grad_(True)
@@ -133,35 +135,58 @@ def main(args):
 
                 for i in range(args.n_way):
                     logits_sum[i].backward(retain_graph=True)
-                    x_grad = torch.unsqueeze(x_copy.grad, 1)  # [batch_size, 1, input_channels, input_height, input_width]
+                    x_grad = x_copy.grad.clone().detach()
+                    #x_grad = x_grad.unsqueeze(1)  # [batch_size, 1, input_channels, input_height, input_width]
                     jacobian[:, i, :, :, :] = x_grad
 
-                jacobian = torch.reshape(jacobian, (x.shape[0], y.shape[1], -1))
+                # print(jacobian.shape, x.shape[0], args.n_way)
+                jacobian = torch.reshape(jacobian, (x.shape[0], args.n_way, -1)).to(device)
 
                 # cross entropy 미분 -> y'-y
-                approx_loss = torch.pow(torch.norm(torch.matmul(delta, jacobian.transpose(1, 2))+1.0/args.lipschitz*(pred-y_onehot)),2)
+                approx_loss = torch.pow(torch.norm(torch.matmul(delta, jacobian.transpose(1, 2))+1.0/args.lipschitz*(softmax-y_onehot)),2)
 
             elif args.step==4: 
                 # approximation #1-2
                 # linear approximation + heuristic
-                approx_loss = torch.pow((1.0+args.train_eps)/args.lipschitz*torch.norm(pred-y_onehot), 2)
+                logit = model(x)
+                softmax = F.softmax(logit, dim=1)
+
+                approx_loss = torch.pow((1.0+args.train_eps)/args.lipschitz*torch.norm(softmax-y_onehot), 2)
 
             elif args.step==5:
                 # minus upper bound
-                approx_loss = loss-1.0/(2*args.lipschitz)*torch.pow(torch.norm(pred-y_onehot),2)
+                at = utils.setAttack("PGD_Linf", model, args.train_eps, args)
+                x_adv = at.perturb(x, y)
+                logit_adv = model(x_adv)
+                loss_adv = torch.nn.functional.cross_entropy(logit_adv, y, reduction='none')
+
+                logit = model(x)
+                softmax = F.softmax(logit, dim=1)
+
+                approx_loss = loss-1.0/(2*args.lipschitz)*torch.pow(torch.norm(softmax-y_onehot),2)
                 bound_rate_elements += x.shape[0]
                 bound_rate += (loss_adv<=approx_loss).sum().item() 
 
             elif args.step==6:
                 # plus upper bound
-                approx_loss = loss+3.0/(2*args.lipschitz)*torch.pow(torch.norm(pred-y_onehot),2)
+                at = utils.setAttack("PGD_Linf", model, args.train_eps, args)
+                x_adv = at.perturb(x, y)
+                logit_adv = model(x_adv)
+                loss_adv = torch.nn.functional.cross_entropy(logit_adv, y, reduction='none')
+
+                logit = model(x)
+                softmax = F.softmax(logit, dim=1)
+
+                approx_loss = loss+3.0/(2*args.lipschitz)*torch.pow(torch.norm(softmax-y_onehot),2)
                 bound_rate_elements += x.shape[0]
                 bound_rate += (loss_adv<=approx_loss).sum().item() 
-
-            train_loss_adv += approx_loss * x.shape[0]
-            adv_optim.zero_grad()
-            approx_loss.backward()
-            adv_optim.step()
+            
+            if args.step>0:
+                approx_loss = approx_loss.sum()
+                train_loss_adv += approx_loss.item() * x.shape[0]
+                adv_optim.zero_grad()
+                approx_loss.backward()
+                adv_optim.step()
 
         if bound_rate_elements>0:
             writer.add_scalar("bound_rate", round(bound_rate/bound_rate_elements*100,4), epoch)
@@ -184,7 +209,7 @@ def main(args):
             outputs = torch.argmax(pred, dim=1)
             val_correct_count += (outputs == y).sum().item()
             loss = torch.nn.functional.cross_entropy(logit, y)
-            val_loss += loss*x.shape[0]
+            val_loss += loss.item()*x.shape[0]
 
         at = utils.setAttack(args.test_attack, model, args.test_eps, args)
         for _, (x, y) in enumerate(tqdm(db_val, desc="adv val")):
@@ -196,13 +221,15 @@ def main(args):
             outputs = torch.argmax(pred, dim=1)
             val_adv_correct_count += (outputs == y).sum().item()
             loss = torch.nn.functional.cross_entropy(logit, y)
-            val_loss_adv == loss*x.shape[0]
+            val_loss_adv == loss.item()*x.shape[0]
 
         writer.add_scalar("val/acc", round(val_correct_count/len(valid_data)*100, 4), epoch)
         writer.add_scalar("val/loss", round(val_loss/len(valid_data)*100, 4), epoch)
         writer.add_scalar("val/adv_acc", round(val_correct_count/len(valid_data)*100, 4), epoch)
         writer.add_scalar("val/adv_loss", round(val_loss/len(valid_data)*100, 4), epoch)
-    
+
+        scheduler.step()
+        adv_scheduler.step()
 
 
 if __name__ == '__main__':
@@ -216,15 +243,15 @@ if __name__ == '__main__':
     
     # Training options
     argparser.add_argument('--model', type=str, help='type of model to use', default="")
-    argparser.add_argument('--epoch', type=int, help='epoch number', default=50)
+    argparser.add_argument('--epoch', type=int, help='epoch number', default=100)
     argparser.add_argument('--task_num', type=int, help='meta batch size, namely task num', default=256)
     argparser.add_argument('--device_num', type=int, help='what gpu to use', default=0)
-    argparser.add_argument('--lr', type=float, help='learning rate', default=0.0005)
+    argparser.add_argument('--lr', type=float, help='learning rate', default=0.01)
     argparser.add_argument('--lr_ratio', type=float, help='adv lr ratio', default=0.5)
 
     # NAM options
     argparser.add_argument('--lipschitz', type=float, help='lipschitz constant', default=4/27) # 계산한 값을 토대로
-    argparser.add_argument('--step', type=int, help='step in proof', defualt=1)
+    argparser.add_argument('--step', type=int, help='step in proof', default=1)
     argparser.add_argument('--twice', action='store_true', help='apply standard loss value twice', default=False)
     argparser.add_argument('--train_eps', type=float, help='to check upper bound', default=2.0)
 
